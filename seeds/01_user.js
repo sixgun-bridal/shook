812978c5ec14e50bfe0ebabfd4afd96f011484bd
Tristannica <gilford.tristan@gmail.com>
2017-04-25 20:14:37 -0600

    forgot the s

--- seeds/01_user.js
+++ seeds/01_user.js
@@ -1,10 +1,10 @@
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('user').del()
+  return knex('users').del()
     .then(function () {
       // Inserts seed entries
-      return knex('user').insert([
+      return knex('users').insert([
         {
           first_name: 'Izabela',
           last_name: 'Louw',
