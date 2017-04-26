5ce421b2149ce5eac142c71e763af834ad5a74f8
Tristannica <gilford.tristan@gmail.com>
2017-04-25 20:13:31 -0600

    users

--- migrations/20170424122159_03_comment.js
+++ migrations/20170424122159_03_comment.js
@@ -3,8 +3,8 @@ exports.up = function(knex, Promise) {
   return knex.schema.createTable('comment', function(table) {
     table.increments();
     table.string('content').notNull();
-    table.integer('user_id');
-    table.foreign('user_id').references('user.id');
+    table.integer('users_id');
+    table.foreign('users_id').references('users.id');
     table.integer('bet_id');
     table.foreign('bet_id').references('bet.id');
   })
