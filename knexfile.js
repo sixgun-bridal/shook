4b547e3da6601f31e3f6d57a7f83b8f37d0d973f
Justin Hart <justinhart90@gmail.com>
2017-05-08 12:26:30 -0600

    added production config

--- knexfile.js
+++ knexfile.js
@@ -1,10 +1,15 @@
 // Update with your config settings.
-// require('dotenv').config()
+require('dotenv').config()
 
 module.exports = {
 
   development: {
     client: 'pg',
     connection: 'postgres://localhost/shook'
+  },
+
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL
   }
 }
